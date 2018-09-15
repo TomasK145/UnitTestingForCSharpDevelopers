391b14107b433beff7ca297144ac915b2e582a77
Tomas Krchnak <tomas.krchnak@millennium.sk>
2018-09-15 19:46:24 +0200

    Testing the return type of methods

---
+++ TestNinja.UnitTests/CustomerControllerTests.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class CustomerControllerTests
+    {
+        [Test]
+        public void GetCustomer_IdIsZero_ReturnNotFound()
+        {
+            var controller = new CustomerController();
+
+            var result = controller.GetCustomer(0);
+
+            Assert.That(result, Is.TypeOf<NotFound>()); //overenie ze vysledok je urciteho typu (nie zdedeny objekt)
+            Assert.That(result, Is.InstanceOf<NotFound>());  //znamena ze result moze byt typu NotFound alebo odvodeny typ
+        }
+
+        [Test]
+        public void GetCustomer_IdIsNotZero_ReturnOk()
+        {
+
+        }
+    }
+}
