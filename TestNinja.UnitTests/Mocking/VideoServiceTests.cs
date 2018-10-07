1f80cee51a9b2e8587340613b65928c0ffc42534
Tomas Krchnak <tomas.krchnak@millennium.sk>
2018-10-07 15:12:33 +0200

    VideoService mocking + testing

---
+++ TestNinja.UnitTests/Mocking/VideoServiceTests.cs
@@ -0,0 +1,62 @@
+﻿using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestNinja.Mocking;
+
+namespace TestNinja.UnitTests.Mocking
+{
+    [TestFixture]
+    public class VideoServiceTests
+    {
+        private VideoService _videoService;
+        private Mock<IFileReader> fileReader;
+        private Mock<IVideoRepository> _repository;
+
+        [SetUp]
+        public void SetUp()
+        {
+            fileReader = new Mock<IFileReader>(); //inicializacia mock objektu
+            _repository = new Mock<IVideoRepository>();
+            _videoService = new VideoService(fileReader.Object, _repository.Object);
+        }
+
+        [Test]
+        public void ReadVideoTitle_EmptyFile_ReturnError()
+        {
+            fileReader.Setup(fr => fr.Read("video.txt")).Returns("{\"Title\":\"error\"}"); //definovanie co sa ma stat ak je volana metoda Read s atributom "video.txt" --> vrati string "error"            
+
+            var result = _videoService.ReadVideoTitle();
+
+            Assert.That(result, Does.Contain("error").IgnoreCase);
+        }
+
+        [Test]
+        public void GetUnprocessedVideosAsCsv_AllVideosAreProcessed_ReturnAnEmptyString()
+        {
+            _repository.Setup(r => r.GetUnprocessedVideos()).Returns(new List<Video>());
+
+            var result = _videoService.GetUnprocessedVideosAsCsv();
+
+            Assert.That(result, Is.EqualTo(""));
+        }
+
+        [Test]
+        public void GetUnprocessedVideosAsCsv_FewUnProcessedVideos_ReturnAStringWithIdOgUnprocessedVideos()
+        {
+            _repository.Setup(r => r.GetUnprocessedVideos()).Returns(new List<Video>
+            {
+                new Video {Id = 1},
+                new Video {Id = 2},
+                new Video {Id = 3}
+            });
+
+            var result = _videoService.GetUnprocessedVideosAsCsv();
+
+            Assert.That(result, Is.EqualTo("1,2,3"));
+        }
+    }
+}
--- TestNinja/Mocking/VideoService.cs
+++ TestNinja/Mocking/VideoService.cs
@@ -10,10 +10,12 @@ namespace TestNinja.Mocking
     public class VideoService
     {
         private readonly IFileReader _fileReader;
+        private IVideoRepository _repository;
 
-        public VideoService(IFileReader fileReader)
+        public VideoService(IFileReader fileReader, IVideoRepository repository = null)
         {
             _fileReader = fileReader;
+            _repository = repository ?? new VideoRepository();
         }
 
         public string ReadVideoTitle()
@@ -28,19 +30,12 @@ namespace TestNinja.Mocking
         public string GetUnprocessedVideosAsCsv()
         {
             var videoIds = new List<int>();
-            
-            using (var context = new VideoContext())
-            {
-                var videos = 
-                    (from video in context.Videos
-                    where !video.IsProcessed
-                    select video).ToList();
-                
-                foreach (var v in videos)
-                    videoIds.Add(v.Id);
 
-                return String.Join(",", videoIds);
-            }
+            var videos = _repository.GetUnprocessedVideos();
+            foreach (var v in videos)
+                videoIds.Add(v.Id);
+
+            return String.Join(",", videoIds);
         }
     }
 
