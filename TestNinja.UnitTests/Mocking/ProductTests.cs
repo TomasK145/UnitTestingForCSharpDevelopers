6735d65aaeb7c48e2a0949886962edcd2e841bd6
tomask145 <thomas.krchnak@gmail.com>
2020-02-16 19:06:13 +0100

    mock abuse

--- TestNinja.UnitTests/Mocking/ProductTests.cs
+++ TestNinja.UnitTests/Mocking/ProductTests.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using Moq;
+using NUnit.Framework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,6 +18,20 @@ namespace TestNinja.UnitTests.Mocking
             var product = new Product { ListPrice = 100 };
 
             var result = product.GetPrice(new Customer { IsGold = true });
+
+            Assert.That(result, Is.EqualTo(70));
+        }
+
+
+        [Test]
+        public void GetPrice_GoldCustomer_Apply30PercentDiscount2()
+        {
+            var customer = new Mock<ICustomer>();
+            customer.Setup(c => c.IsGold).Returns(true);
+
+            var product = new Product { ListPrice = 100 };
+
+            var result = product.GetPrice(customer.Object);
 
             Assert.That(result, Is.EqualTo(70));
         }
